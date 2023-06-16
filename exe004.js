924c9459e6aeb42a5221b4c8976568b5201dee59
Minato <59450675+sminato27@users.noreply.github.com>
2023-06-15 21:35:53 -0300

    Adição de exercícios.

---
+++ exe004.js
@@ -0,0 +1,9 @@
+console.log('O console funcionou corretamente!'); // O comando 'console.log' é o mesmo do 'document.write', ou seja, escreve na tela.
+let velocidade = 59.9;
+console.log(`A velocidade do seu carro é: ${velocidade}Km/h`);
+
+if (velocidade >= 60) { // Condição simples, pois não tem um 'else'.
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!');
---
+++ exe005.js
@@ -0,0 +1,9 @@
+let país = 'EUA';
+
+console.log(`Vivendo em ${país}`);
+
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+};
